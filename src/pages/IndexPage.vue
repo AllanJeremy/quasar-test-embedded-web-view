7cb67eff03836f811b9f7a40df4de9bc9e0683a4
Allan Jeremy <allan.jeremy@thisdot.co>
2024-03-12 14:14:16 +0300

    debug

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="q-pa-xl items-center justify-evenly">
     <div class="column q-gutter-y-lg">
       <q-btn outlined to="/another">Vue router go to other</q-btn>
       <q-btn color="primary" @click="goToExternalLink('/another')"
@@ -11,11 +11,45 @@
       <q-btn color="negative" @click="attemptToTriggerSystemBrowser('/another')"
         >External new tab [window.open(link, '_system')]</q-btn
       >
+
+      <a class="q-btn bg-yellow-2" href="/another" target="__blank" download
+        >Open in browser (link with download attr)</a
+      >
+
+      <q-btn color="dark" @click="replaceLocationHref('/another')"
+        >Replacing window.location.href</q-btn
+      >
+      <q-btn
+        color="dark"
+        outline
+        @click="
+          replaceLocationHref(
+            'https://quasar-test-embedded-web-view.vercel.app/another'
+          )
+        "
+        >Replacing window.location.href (external link)</q-btn
+      >
     </div>
+    <hr class="q-my-xl" />
+    <section class="q-mt-lg">
+      <h6 class="q-my-none">Additional data</h6>
+
+      <code>
+        <pre>User agent: {{ userAgent }}</pre>
+        <pre>Is Instagram: {{ userAgentIsInstagram }}</pre>
+      </code>
+    </section>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
+const userAgent = computed(() => navigator.userAgent);
+const userAgentIsInstagram = computed(() =>
+  navigator.userAgent?.includes('Instagram')
+);
+
 function attemptToTriggerSystemBrowser(link: string) {
   window.open(link, '_system');
 }
@@ -25,5 +59,9 @@ function goToExternalLink(link: string, isNewTab = false) {
   }
 
   return window.location.replace(link);
+}
+
+function replaceLocationHref(link: string) {
+  window.location.href = link;
 }
 </script>
