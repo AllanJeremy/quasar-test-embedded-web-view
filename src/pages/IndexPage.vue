9be4e823cf65cdde1d6bc59acd52b6430cd68e04
Allan Jeremy <allan.jeremy@thisdot.co>
2024-03-12 13:56:10 +0300

    test: attempt to open system browser

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,11 +8,17 @@
       <q-btn color="positive" @click="goToExternalLink('/another', true)"
         >External new tab (window.open)</q-btn
       >
+      <q-btn color="negative" @click="attemptToTriggerSystemBrowser('/another')"
+        >External new tab (window.open(link, '_system'))</q-btn
+      >
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+function attemptToTriggerSystemBrowser(link: string) {
+  window.open(link, '_system');
+}
 function goToExternalLink(link: string, isNewTab = false) {
   if (isNewTab) {
     window.open(link, '__blank');
