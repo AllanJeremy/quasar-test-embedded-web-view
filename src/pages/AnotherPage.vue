05ad85ad93acdb245ef4250d538215aaa4a27bf5
Allan Jeremy <allan.jeremy@thisdot.co>
2024-03-12 13:49:26 +0300

    initial commit

---
+++ src/pages/AnotherPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="window-height flex column items-center justify-center bg-yellow-2 q-py-xl"
+  >
+    <h4>This is another page fr</h4>
+    <q-btn color="dark" to="/">Back home</q-btn>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="column q-gutter-y-lg">
+      <q-btn outlined to="/another">Vue router go to other</q-btn>
+      <q-btn color="primary" @click="goToExternalLink('/another')"
+        >External (window.replace)</q-btn
+      >
+      <q-btn color="positive" @click="goToExternalLink('/another', true)"
+        >External new tab (window.open)</q-btn
+      >
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+function goToExternalLink(link: string, isNewTab = false) {
+  if (isNewTab) {
+    window.open(link, '__blank');
+  }
+
+  return window.location.replace(link);
+}
+</script>
